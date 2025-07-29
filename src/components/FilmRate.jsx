262e6c586ba8edac8524ad2b3782d3b6ceedab89
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-07-29 16:12:53 +0200

    fix rate view

--- src/components/FilmRate.jsx
+++ src/components/FilmRate.jsx
@@ -2,24 +2,19 @@
 export default function FilmRate({ element }) {
     let voteTo5 = Math.round(element.vote_average / 2)
 
-
-    {
-        for (let i = 0; i < voteTo5; i++) {
-
-            return (
-                <i className="fa-solid fa-star"></i>
-            )
-        }
-
-        for (let i = 0; i < (5 - voteTo5); i++) {
-
-            return (
-                <i className="fa-regular fa-star"></i>
-            )
-        }
-
-    }
-
+    return (
+        <div>
+            {Array.from({ length: 5 }).map((_, i) => (
+                <span key={i}>
+                    {i < Math.round(voteTo5) ? (
+                        <i class="fa-solid fa-star text-warning"></i>
+                    ) : (
+                        <i class="fa-regular fa-star text-warning"></i>
+                    )}
+                </span>
+            ))}
+        </div>
+    );
 
 
 }
