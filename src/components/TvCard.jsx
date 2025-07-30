a557b387e9b71ba798a37d6f77b9bcd25d2a7a3a
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-07-30 10:29:51 +0200

    fix overflow card text

--- src/components/TvCard.jsx
+++ src/components/TvCard.jsx
@@ -5,18 +5,24 @@ export default function TvCard({ element }) {
         <div className="col-2">
             <div className="card text-bg-dark h-100">
                 <img src={`https://image.tmdb.org/t/p/w342/${element.poster_path}`} className="card-img" alt={element.original_language} />
-                <div className="card-img-overlay">
-                    <h5 className="card-title">{element.name.toUpperCase()}</h5>
-                    <h6 className="card-subtitle mb-2 ">{element.original_name}</h6>
-                    <img
+                <div className="card-img-overlay fs-6 g-0 ">
+                    <div>Titolo: <b className="card-title">{element.name.toUpperCase()}</b></div>
+                    <div>Titolo Originale: <b className="card-subtitle mb-2">{element.original_name}</b></div>
+                    <img className="img_card"
                         src={`https://flagcdn.com/16x12/${element.original_language}.png`}
                         width="16"
                         height="12"
                         alt={element.original_language} />
-                    <div className="d-flex">
-                        {<FilmRate element={element} />}
+                    <div className="d-flex"> Voto:
+                        <div className="d-flex">
+                            {<FilmRate element={element} />}
 
+                        </div>
                     </div>
+                    <div>
+                        Overview: <p className="fs-6">{element.overview}</p>
+                    </div>
+
                 </div>
             </div>
         </div>
