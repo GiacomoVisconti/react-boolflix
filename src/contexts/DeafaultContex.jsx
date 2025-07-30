7c4e50b9e968fa41df65637c6c3efae35b750935
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-07-30 16:00:52 +0200

    fetch for movie and tvseries genres in ContextDefault

--- src/contexts/DeafaultContex.jsx
+++ src/contexts/DeafaultContex.jsx
@@ -1,10 +1,12 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 const MovieTvContext = createContext()
 
 function MovieTvProvider({ children }) {
 
     const [movieData, setMovieData] = useState(null)
     const [tvData, setTvData] = useState(null)
+    const [movieGenres, setMovieGenres] = useState(null)
+    const [tvGenres, setTvGenres] = useState(null)
 
     function onSearchClick(searchTitle) {
         let string = searchTitle?.replace(" ", "+")
@@ -32,11 +34,30 @@ function MovieTvProvider({ children }) {
 
     }
 
+    useEffect(() => {
+        const movie_genres_url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_API_KEY}`
+        const tv_genres_url = `https://api.themoviedb.org/3/genre/tv/list?api_key=${import.meta.env.VITE_API_KEY}`
+
+        fetch(movie_genres_url)
+            .then(res => res.json())
+            .then(data => {
+                setMovieGenres(data)
+            })
+        fetch(tv_genres_url)
+            .then(res => res.json())
+            .then(data => {
+                setTvGenres(data)
+            })
+
+
+
+    }, [])
+
 
     return (
         <MovieTvContext.Provider
             value={{
-                onSearchClick, movieData, tvData,
+                onSearchClick, movieData, tvData, movieGenres, tvGenres,
             }}>
 
             {children}
